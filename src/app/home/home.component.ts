d0a8c3fe7dbcd0e48aa0cb545c7895a4ed27a087
beattris <beatriceebirim@gmail.com>
2023-12-14 11:01:12 +0100

    built a custom observable, set up both the subscribe and unsubscribe method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,35 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Observable, Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  private firstObsSubscription: Subscription
+  count = 0;
+  private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
-    })
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+
+    const customIntervalObservable = new Observable(observer => {
+      setInterval(() => {
+        observer.next(this.count);
+        this.count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
+    });
   }
 
   ngOnDestroy(): void {
-    this.firstObsSubscription.unsubscribe(); 
+    this.firstObsSubscription.unsubscribe();
   }
-
 }
